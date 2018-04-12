3fa79048e3a002a2eb0af5765d29ba5ad96f021e
cxz1418 <cxz1418@ajou.ac.kr>
2018-04-12 23:09:36 +0900

    com1

--- nets/rnn.py
+++ nets/rnn.py
@@ -30,14 +30,33 @@ class RNNSampleModel(BaseModel):
 
 
         with tf.variable_scope('mainRNN', reuse=reuse):
-            cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden, state_is_tuple=True, activation=tf.tanh,
+
+            ######bidirectional_rnn######
+            cell_1 = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden/2, state_is_tuple=True, activation=tf.tanh,
                                                   reuse=reuse);
+            cell_2 = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden/2, state_is_tuple=True, activation=tf.tanh,
+                                                  reuse=reuse);
+            outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_1, cell_2, input, dtype=tf.float32);
+
+            outputs = tf.concat([outputs[0], outputs[1]], axis=2)
+
+
+
+            '''
+            ######dynamic_rnn######
+
+            #GRUCell
+            #cell = tf.nn.rnn_cell.GRUCell(num_units=self.n_hidden, activation=tf.tanh,
+            #                                    reuse=reuse);
+
+            #LSTMCell
+            cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden, state_is_tuple=True, activation=tf.tanh,
+                                                       reuse=reuse);
             outputs, states = tf.nn.dynamic_rnn(cell, input, dtype=tf.float32)
+            '''
 
-            outputs = tf.transpose(outputs, [1, 0, 2])
-            outputs = outputs[-1]
-
-
+            #using only last step
+            outputs = outputs[:,-1]
             outputs = tf.matmul(outputs, W) + b
 
             cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=outputs, labels=expected))
